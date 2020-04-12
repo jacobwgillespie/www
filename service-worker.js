c56edd5b6b32658071f787442382a58847f9442a
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-04-12 15:10:37 +0100

    Generate service worker directly

--- service-worker.js
+++
@@ -1,3 +0,0 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
-
-workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
--- workbox-config.js
+++ workbox-config.js
@@ -2,5 +2,4 @@ module.exports = {
   globDirectory: 'public/',
   globPatterns: ['**/*.{html,png,pub,jpg,txt,js,xml,css}'],
   swDest: 'public/service-worker.js',
-  swSrc: 'service-worker.js',
 }
