78f0364699130d278e6c465b98898a5b59db7d6e
Jacob Gillespie <jacobwgillespie@gmail.com>
2020-04-12 15:05:12 +0100

    Upgrade workbox

--- service-worker.js
+++ service-worker.js
@@ -1,3 +1,3 @@
-importScripts('https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js')
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js')
 
-workbox.precaching.precacheAndRoute([])
+workbox.precaching.precacheAndRoute(self.__WB_MANIFEST)
