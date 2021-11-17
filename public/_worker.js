5a205751e632c16e043a4556239310926fb2123b
Jacob Gillespie <jacobwgillespie@gmail.com>
2021-11-17 18:00:06 +0000

    Rewrite service worker

---
+++ public/_worker.js
@@ -0,0 +1,14 @@
+export default {
+  /**
+   * @param {Request} request
+   * @param {*} env
+   */
+  async fetch(request, env) {
+    if (req.url.startsWith('/service-worker.js')) {
+      const next = new Request('/static/service-worker.js', request)
+      return fetch(next)
+    }
+
+    return env.ASSETS.fetch(request)
+  },
+}
